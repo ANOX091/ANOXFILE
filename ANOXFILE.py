d93af21fa44c436dcf3c54344164867f535feb50
ANOX091 <152467512+ANOX091@users.noreply.github.com>
2023-11-30 12:31:04 +0530

    Update ANOXFILE.py

--- ANOXFILE.py
+++ ANOXFILE.py
@@ -553,7 +553,6 @@ def passwrd():
                     pwv.append('57273200')
                     pwv.append(frs+'123')
                     pwv.append(frs+'1234')
-                    pwv.append(frs+'12345')
                     pwv.append(nmf)
                     pwv.append(frs)
                     pwv.append(frs+'@123')
@@ -565,9 +564,8 @@ def passwrd():
                     pwv.append('57273200')
                     pwv.append(frs+'123')
                     pwv.append(frs+'1234')
-                    pwv.append(frs+'12345')
                     pwv.append(nmf)
-                    pwv.append('57273200')
+                    pwv.append('59039200')
                     pwv.append(frs+'@123')
                     pwv.append(frs+'@1234')
             if 'ya' in pwpluss:
